1f5ac0b62c0726f85017733d3f03acfdb3c4ec86
Tedd OKANO <okano.akifumi@gmail.com>
2025-05-12 03:09:26 +0900

    image links on visitor list

--- CGI-Executables/visitors.py
+++ CGI-Executables/visitors.py
@@ -16,6 +16,12 @@ image_folder_web	= "/img/"
 excel_output_file	= "../Documents/visitors.xlsx"
 
 
+def link_to_image( tag_id ):
+	if os.path.isfile( f"{image_folder_access}{tag_id}.jpg" ):
+		return f"<td><a href = '{image_folder_web}{tag_id}.jpg'>{k}<img src='{image_folder_web}{tag_id}.jpg' width='200' alt='{tag_id}'></td></a>"
+	else:	
+		return f"<td><a href = '{image_folder_web}{tag_id}.jpg'>{k}</td></a>"
+
 scripts = """
     <link href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.min.css" rel="stylesheet"/>
     <link href="https://cdn.datatables.net/v/zf/jq-3.6.0/dt-1.13.4/b-2.3.6/b-html5-2.3.6/date-1.4.1/fh-3.3.2/sb-1.4.2/datatables.min.css" rel="stylesheet"/>
@@ -82,7 +88,7 @@ h	= h.replace( "===LOG_TABLE===",  scripts + total_log.to_html( classes = "my-ta
 h	= h.replace( "===DOWNLOAD_LINK===", '<p><a href = "/visitors.xlsx">Download log file</a></p>' )
 
 for k in tags:
-	h	= h.replace( f"<td>{k}</td>", f"<td><a href = '{image_folder_web}{k}.jpg'>{k}</a></td>" )
+	h	= h.replace( f"<td>{k}</td>", link_to_image( k ) )
 
 print( h )
 

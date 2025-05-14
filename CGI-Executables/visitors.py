5e1f0aa09d988bf1c685adbb8222fe8e3cacf219
Tedd OKANO <okano.akifumi@gmail.com>
2025-05-15 04:55:51 +0900

    visitor.py simplify and "total" column placed next to visitor data

--- CGI-Executables/visitors.py
+++ CGI-Executables/visitors.py
@@ -61,10 +61,8 @@ log_data.fillna( "", inplace = True )
 log_data[ "tag_id" ].map( lambda x: int( x ) if type( x ) is float else x )
 
 pv	= pd.pivot_table( log_data, index = "tag_id", columns = "demo_id", values = "time", aggfunc = "count" )
-pv.fillna( 0, inplace = True )
-pv.replace( 0, "", inplace = True )
-pv = pv.apply( lambda col: col.map( lambda x: 1 if x != "" else 0 ) )
-pv[ "total" ] = pv.sum(axis=1)
+pv = pv.apply( lambda col: col.map( lambda x: 1 if (x != float( "NaN" )) and (x > 0) else 0 ) )
+pv.insert( 0, "total", pv.sum( axis = 1) )
 
 ###
 ### load visitor info

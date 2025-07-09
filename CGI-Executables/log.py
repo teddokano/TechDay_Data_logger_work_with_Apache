770199280f725418bebcaa5466bfb2700c0de41e
Tedd OKANO <okano.akifumi@gmail.com>
2025-07-09 18:13:22 +0900

    to make log.py work in isolated local network (to include CSS and JS)

--- CGI-Executables/log.py
+++ CGI-Executables/log.py
@@ -12,7 +12,7 @@ page_template_path	= "page_template/log_page.html"
 access_log_folder	= "access_log/"
 excel_output_file	= "../Documents/log.xlsx"
 
-scripts = """
+scripts_net = """
     <link href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.min.css" rel="stylesheet"/>
     <link href="https://cdn.datatables.net/v/zf/jq-3.6.0/dt-1.13.4/b-2.3.6/b-html5-2.3.6/date-1.4.1/fh-3.3.2/sb-1.4.2/datatables.min.css" rel="stylesheet"/>
  
@@ -30,6 +30,23 @@ scripts = """
     </script>
 """
 # https://qiita.com/TKfumi/items/05fc0208014a83fb8079
+scripts = """
+    <link href="/styles/foundation.min.css" rel="stylesheet"/>
+    <link href="/styles/datatables.min.css" rel="stylesheet"/>
+ 
+    <script src="/styles/datatables.min.js"></script>
+    <script src="/styles/foundation.min.js"></script>
+
+    <script>
+        $(document).ready(function() {$('.my-table').DataTable({
+            select: true,
+            displayLength: 25,
+            buttons: ['copy'],
+            fixedHeader: true,
+            dom: 'iQrtBlp',
+        });})
+    </script>
+"""
 
 
 class Access:
